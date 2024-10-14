ae3b12e65bc09b92f61cf8a8f4e83eba07535eed
21R01A7263 <21r01a7263@cmritonline.ac.in>
2024-10-14 08:24:45 -0700

    nav

---
+++ app/NavBar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+import { RiGitBranchFill } from "react-icons/ri";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <RiGitBranchFill className="h-6 w-6" />
+      </Link>
+      <ul className="flex space-x-3">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-800 hover:font-bold transition-colors"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
