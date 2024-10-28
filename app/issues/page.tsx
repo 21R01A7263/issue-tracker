1ea97e4990c98bfad6975f7f618fe6b449193f31
21R01A7263 <21r01a7263@cmritonline.ac.in>
2024-10-28 13:10:56 +0530

    tabulate issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,15 +1,54 @@
-"use client";
-
 import React from "react";
 import Link from "next/link";
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
   return (
     <div>
-      <Button>
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">New Issue</Link>
+        </Button>
+      </div>
+
+      <Table.Root variant="surface" className="max-w-2xl">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell justify={"center"}>
+              Issue
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell
+              justify={"center"}
+              className="hidden md:table-cell"
+            >
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell
+              justify={"center"}
+              className="hidden md:table-cell"
+            >
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell align="center">
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+              </Table.Cell>
+              <Table.Cell align="center" className="hidden md:table-cell">
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell align="center" className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
